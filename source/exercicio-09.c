c4891865f64cc783e8323deb64b4e1e53f304b86
Itamar Rocha <itamar.itamar@discente.ufg.br>
2023-07-27 20:28:45 -0300

    verification logic now works

--- source/exercicio-09.c
+++ source/exercicio-09.c
@@ -1,27 +1,27 @@
 // Exercício 9
 
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
-    bool input = true;
-    float notas[2], media = 0;
-    do {
-    printf("Digite as duas notas: ");
-    scanf("%f %f", &notas[0], &notas[1]);
-    for (int i = 0; i < 2; i++) {
-        if (notas[i] >= 0 && notas[i] <= 10){
-            media += notas[i];
-        } else {
-            break;
-        }
-    }
-    input = false;
-    } while (input);
-    
-    media = media/2;
-    printf("Sua média é: %.2f", media);
-
-    return 0;
+	float notas[2], media = 0;
+		while(1){
+			printf("Digite as duas notas: ");
+			scanf("%f %f", &notas[0], &notas[1]);
+			int i;
+			for (i = 0; i < 2; i++) {
+				if (notas[i] >= 0 && notas[i] <= 10){
+					media += notas[i];
+				} else {
+					printf("As notas informadas devem entre 0 e 10!\n");
+					break;
+				}
+			}
+			if (i == 2){
+				break;
+			}
+		}
+	media = media/2;
+	printf("Sua média é: %.2f", media);
+	return 0;
 }
